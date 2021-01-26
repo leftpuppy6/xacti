3aa236aca713d7d3cfbcbe9332635c192dd46f34
leftpuppy6 <takuma.bosendorfer.0021@docomo.ne.jp>
2021-01-27 01:26:14 +0900

    fix: changed the condition method from switch to if

--- src/message/handleMessage.ts
+++ src/message/handleMessage.ts
@@ -5,15 +5,11 @@ import {createInvite} from "./invite"
 import {prefix} from "../env"
 
 export function handleMessage(message:Message):void {
-  switch (message.content) {
-    case `${prefix} help`:
-      help(message)
-      break;
-    case `${prefix} invite`:
-      createInvite(message)
-      break;
-    default:
-      message.channel.send("xacti doesn't have the command you typed.")
-      break;
+  if (message.content === `${prefix} help`) {
+    help(message)
+  } else if (message.content === `${prefix} invite`) {
+    createInvite(message)
+  } else {
+    message.channel.send("xacti doesn't have the command you typed.");
   }
 }
