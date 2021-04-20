4419f9347f41624b6250a9a745bd662296bfc1fa
tkeiyama <thoams18thx@gmail.com>
2021-04-20 17:21:01 +0900

    Added the version command

---
+++ src/commands/version.ts
@@ -0,0 +1,10 @@
+import {Message} from "discord.js"
+
+export = {
+    name: 'version',
+    description: "Shows xacti's version.",
+    execute(message:Message) {
+        const version = require("../../package.json").version
+        message.channel.send(`v${version}`)
+    }
+}
