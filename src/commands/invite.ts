8cd1a59a8ac3a9c9d72e0067fe59ca5392718a3b
Tomoaki Keiyama <50320710+tkeiyama@users.noreply.github.com>
2021-04-22 08:22:25 +0900

    Modified words

--- src/commands/invite.ts
+++ src/commands/invite.ts
@@ -2,7 +2,7 @@ import { Message } from 'discord.js'
 
 export = {
     name: 'invite',
-    description: "Send an invite link of xacti.",
+    description: "Sends an invitation link of xacti.",
     execute(message: Message) {
         message.channel.send('https://discord.com/api/oauth2/authorize?client_id=792416136318287882&permissions=8&scope=bot')
     }
