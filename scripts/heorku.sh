04ae727feeec89a93000527b5ae1a271489fdead
tkeiyama <thomas18thx@gmail.com>
2021-04-27 18:38:53 +0900

    Add a script for heroku

---
+++ scripts/heorku.sh
@@ -0,0 +1,19 @@
+echo "Start setup"
+rm -rf .gitignore
+echo "End setup"
+
+echo "Start build"
+yarn build
+echo "End build"
+
+echo "Start git"
+git config --global user.name "xacti"
+git config --global user.email "xacti@email.com"
+rm -rf .git
+git init
+git checkout -b heroku
+git add dist
+git add assets
+git commit -m "Update files"
+git push origin -f origin heroku
+echo "End git"
