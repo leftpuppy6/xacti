87de5fe743248b7c4e843f4619a905bcf5cff3a8
Tomoaki Keiyama <50320710+tkeiyama@users.noreply.github.com>
2021-05-01 13:19:08 +0900

    Update the heroku script

--- scripts/heroku.sh
+++ scripts/heroku.sh
@@ -15,7 +15,7 @@ git config --global user.email "xacti@email.com"
 git checkout --orphan heroku
 git reset --hard
 rm -rf node_modules
-git checkout master package.json Profile yarn.lock
+git checkout master package.json Procfile yarn.lock
 git add dist package.json Procfile yarn.lock assets
 git commit -m "Push to heroku"
 git push -f origin heroku
