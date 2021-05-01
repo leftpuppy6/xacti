ff58191ffe50ca5a8e9a164195f2337ffa324260
tkeiyama <thomas18thx@gmail.com>
2021-05-01 17:47:00 +0900

    Add prebuild script

--- scripts/createAssets.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-FOLDER="assets"
-HELP_MESSAGE="helpMessage"
-
-if [ ! -e $FOLDER ]; then
-  mkdir assets
-  touch assets/$HELP_MESSAGE
-fi
---
+++ scripts/prebuild.sh
@@ -0,0 +1,27 @@
+#!/bin/bash
+
+ASSETS="assets"
+HELP_MESSAGE="helpMessage"
+
+DIST="dist"
+SRC="src"
+
+NODE_ENV=`env | grep NODE_ENV`
+
+# Create an assets folder.
+if [ ! -e $ASSETS ]; then
+  mkdir $ASSETS
+  touch $ASSETS/$HELP_MESSAGE
+fi
+
+# Create the help message.
+yarn ts-node-dev scripts/createHelpMessage.ts
+
+# Move the assets folder to the dist/src folder.
+if [ "$NODE_ENV" = "production" ]; then
+  rm -rf $DIST/$ASSETS
+  mv $ASSETS $DIST/$ASSETS
+else
+  rm -rf $SRC/$ASSETS
+  mv $ASSETS src
+fi
